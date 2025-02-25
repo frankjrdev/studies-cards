3a8c812fa343dbccc6267a7dbf7d168497808c27
Francisco Quintero <frankjrdev.com>
2025-02-25 18:17:02 +0100

    Add Swagger documentation setup for API

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Api study flash cards')
+    .setDescription('Documentation of the api study flash cards')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('study-flash-cards')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
