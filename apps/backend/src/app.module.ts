432ce70317a55465517c2353facdb1c391e38b99
Francisco Quintero <frankjrdev.com>
2025-02-25 18:16:28 +0100

    Add Users, Projects, and Cards modules to the main application module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule, ProjectsModule, CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
