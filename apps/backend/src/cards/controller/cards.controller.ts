0ee4775efa7a4955c951b57473a0fa34917e36e1
Francisco Quintero <frankjrdev.com>
2025-02-25 18:23:36 +0100

    Add Swagger documentation for card creation and retrieval endpoints

--- apps/backend/src/cards/controller/cards.controller.ts
+++ apps/backend/src/cards/controller/cards.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ProjectsService } from '../../projects/service/projects.service';
 import { CardsService } from '../service/cards.service';
 
@@ -9,6 +10,9 @@ export class CardsController {
     private readonly projectsService: ProjectsService,
   ) {}
 
+  @ApiOperation({ summary: 'Create new card' })
+  @ApiResponse({ status: 201, description: 'Flash-card creada exitosamente' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos' })
   @Post()
   async createCard(
     @Body() body: { question: string; answer: string; projectId: number },
@@ -20,6 +24,9 @@ export class CardsController {
     return this.cardsService.createCard(body.question, body.answer, project);
   }
 
+  @ApiOperation({ summary: 'Get cards by project' })
+  @ApiResponse({ status: 201, description: 'consulta exitosa' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos' })
   @Patch(':cardId')
   async updateCardStats(
     @Param('cardId') cardId: number,
